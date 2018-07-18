1baf6f55e1fd319142fa7ec34ba3aea70332d95a
sharde <sharde.m.smith@gmail.com>
2018-07-18 08:55:50 -0500

    lab3

--- src/evenOdd.java
+++ src/evenOdd.java
@@ -10,28 +10,33 @@ public class evenOdd {
 		userNum = scnr.nextInt();
 		
 		if (userNum % 2==0) {
-			System.out.println("The integer " + userNum + " is even. ");
-
-				
-		}else if (userNum % 2!=0) {
-			System.out.println("The integer " + userNum + " is odd. ");
-		
 			if (userNum>=2 && userNum<=25) {
 			System.out.println("Even and less than 25. ");
 		}
-		if (userNum>=26 && userNum<=60){
+			else if (userNum>=26 && userNum<=60){
 			System.out.println("Even");
 		}
-	if (userNum>60) {
+			else if (userNum>60) {
 		System.out.println(userNum + " Even. ");
 	}
+			
+
+				
+		}else if (userNum % 2!=0) {
+			if (userNum > 60) {
+				System.out.println(userNum + " is odd and over 60. ");
+			}
+			else {
+			System.out.println("The integer " + userNum + " is odd. ");
+		}
+		
+			
 	
-	else if (userNum > 60) {
-		System.out.println(userNum + " is odd and over 60. ");
+	
 	}
 	}
 	}
-}	
+
 		
 		
 		
